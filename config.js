39ceba5f595b37d0058c241f0b8263278869fdb7
SupermaxiMarket <monsunrise@gmail.com>
2025-02-06 09:48:31 +0100

    Ajout du fichier de configuration

---
+++ config.js
@@ -0,0 +1,8 @@
+// Configuration des APIs
+const config = {
+    WEATHER_API_KEY: 'VOTRE_CLE_OPENWEATHERMAP',
+    MAPBOX_TOKEN: 'VOTRE_TOKEN_MAPBOX',
+    TRANSPORT_API_KEY: 'VOTRE_CLE_TRANSPORT',
+};
+
+export default config;
