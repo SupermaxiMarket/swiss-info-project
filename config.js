9a09c5958a10ba65f7c4c5f136f00f19f052b93d
SupermaxiMarket <monsunrise@gmail.com>
2025-02-06 10:31:31 +0100

    Ajout du token Mapbox

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration des APIs
 const config = {
     WEATHER_API_KEY: '0300ccf18be7a719523e94b7daf5b6e2',
-    MAPBOX_TOKEN: '', // À compléter avec votre token MapBox
+    MAPBOX_TOKEN: 'pk.ey31Ijoid21ubmVyL',  // Votre token Mapbox
     TRANSPORT_API_KEY: '', // À compléter avec votre clé Transport API
 };
 
