76d5112ac15cd2ff067c0fd2e75307eb17feb866
SupermaxiMarket <monsunrise@gmail.com>
2025-02-06 15:29:32 +0100

    Mise à jour de la configuration

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Configuration des APIs
 const config = {
     WEATHER_API_KEY: '0300ccf18be7a719523e94b7daf5b6e2',
-    MAPBOX_TOKEN: 'pk.ey31Ijoid21ubmVyL',  // Votre token Mapbox
-    TRANSPORT_API_KEY: '', // À compléter avec votre clé Transport API
+    MAPBOX_TOKEN: 'pk.ey31Ijoid21ubmVyL',  // Token incomplet
+    TRANSPORT_API_KEY: '', // À compléter si nécessaire
 };
 
 export default config;
